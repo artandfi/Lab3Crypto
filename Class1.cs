36eec752bf871569ae80ea97923346f5a0746fb9
MaksymRud <mr.freiermann01@gmail.com>
2021-05-16 19:12:24 +0300

    Server

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCrypto
+{
+    class JsonData
+    {
+        private string hash;
+        private string messageBase64;
+        private string sender;
+        private string reciever;
+
+        public JsonData(string hash, string messageBase64, string sender, string reciever)
+        {
+            this.hash = hash;
+            this.messageBase64 = messageBase64;
+            this.sender = sender;
+            this.reciever = reciever;
+        }
+    }
+}
